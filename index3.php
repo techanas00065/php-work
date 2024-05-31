05e69926524bd1fd3e498a0409adf05f638c5ece
Anas khan <162820616+techanas00065@users.noreply.github.com>
2024-05-31 20:21:46 +0500

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,82 @@
+
+
+<?php
+
+// $num = 10;
+// for($i=1;$i<=10; $i++)
+// {
+//     echo $num . "x" ."$i"."=" . $num*$i;
+//     echo"</br>"; 
+// }
+
+// function EvenOdd($number) {
+//     if ($number % 2 == 0) {
+//         return "The number 2 is even.";
+//     } else {
+//         return "The number $number is odd.";
+//     }
+// }
+
+// echo EvenOdd(2);
+
+// for($i = 1; $i<= 100 ; $i++)
+// {
+//     if($i % 3 == 0 && $i % 5 == 0)
+//     {
+//         echo "FizzBuzz";
+//     }
+//     else if($i % 3 == 0)
+//     {
+//         echo "Fizz";
+//     }
+//     else if($i % 5 == 0)
+//     {
+//         echo "Buzz";
+//     }
+//     else
+//     {
+//         echo $i;
+//     }
+//     echo "</br>";
+// }
+
+
+for($a = 1; $a<= 5 ; $a++){
+    for ($i = $a ; $i<=5 ; $i++)
+{echo "*";}
+
+
+ 
+echo "</br>";}
+
+
+
+for($a = 1; $a<= 5 ; $a++){
+    for ($i = 1 ; $i<=$a ; $i++)
+{echo "*";}
+
+
+ 
+echo "</br>";}
+
+
+
+for($i = 0; $i<= 5 ; $i++){
+    for ($x = 0 ; $x<=$i ; $x++)
+    
+{echo $x;}
+
+ 
+echo "</br>";}
+
+
+
+for($i = 1; $i<= 10 ; $i++){
+    echo 11*$i;
+    echo "</br>";}
+
+
+
+
+
+?>
